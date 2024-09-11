72726cc53ada152265c3af22f567508b2364ede1
Richi <tanrichi@hotmail.com>
2024-09-11 00:18:49 -0400

    final basic configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,22 @@ vim.opt.incsearch = true
 
 vim.opt.scrolloff = 25
 vim.opt.updatetime = 50
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,29 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-    	dependencies = { 'nvim-lua/plenary.nvim' },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+	  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+	  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+	  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>fe', function()
+        builtin.find_files({ hidden = true, find_command = { "rg", "--files", "--glob", ".env*" } })
+      end)
 
-	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		vim.keymap.set('n', '<leader>fe', function()
-            		builtin.find_files({ hidden = true, find_command = { "rg", "--files", "--glob", ".env*" } })
-        	end)
-	end
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
