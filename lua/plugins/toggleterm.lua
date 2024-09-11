10fc1d3a4032d73709ae276b1780c51efbb8dcb5
Richi <tanrichi@hotmail.com>
2024-09-11 12:36:18 -0400

    Added plugin ToggleTerm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+      })
+    end,
+  },
+}
