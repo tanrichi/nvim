3aa12058385562436a6e7db0012e3cbfec259203
richi <tanrichi@proton.me>
2024-12-27 01:47:08 -0500

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = {
+      enabled = false,
+    },
     servers = {
       ts_ls = {
         init_options = {
           plugins = {
             {
               name = "@vue/typescript-plugin",
-              location = "/home/richi/.nvm/versions/node/v20.18.0/lib/node_modules/@vue/language-server",
+              location = "/home/richi/.nvm/versions/node/v20.18.1/lib/node_modules/@vue/language-server",
               languages = { "javascript", "typescript", "vue" },
             },
           },
