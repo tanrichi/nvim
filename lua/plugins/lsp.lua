11ff484ba6f36065bec91f81d625ac0299140f86
richi <tanrichi@proton.me>
2024-12-31 18:03:34 -0500

    volar lsp fixed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,25 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = {
-      enabled = false,
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = {
+			enabled = false,
+		},
+		servers = {
+			volar = {},
+			ts_ls = {
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vim.fn.stdpath("data")
+								.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+							languages = { "vue" },
+						},
+					},
+				},
+			},
+			vtsls = false,
+		},
+	},
 }
